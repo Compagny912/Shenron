45f214e76151666a45e1e79511584cb36ce15e1d
Proelium <Compagny912@users.noreply.github.com>
2017-01-08 19:24:37 +0100

    Update SummonListener.java

--- src/main/java/org/sdd/shenron/SummonListener.java
+++ src/main/java/org/sdd/shenron/SummonListener.java
@@ -28,7 +28,7 @@ public class SummonListener implements IMessageListener
             Shenron.get().sendMessage(mdBold("UN HUMAIN TEL QUE VOUS N'EST PAS APTE A FAIRE APPARAITRE SHENRON !"), event.getConversation());
         }
         
-        if (distance < 5 || message.replace("î", "i").contains("listencompagny"))
+        if (distance < 5 || message.contains("listencompagny"))
         {
             Shenron.get().sendMessage(mdBold("@Compagny912 est un dieu, écoutez le!"), event.getConversation());
         }
